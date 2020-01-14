df8ccb131570eb23f460394f5ce0f3788e137f28
theallegrarr <nurudeen.amedu@gmail.com>
2020-01-14 08:59:49 +0100

    chore: configured linting

--- api/server.js
+++ api/server.js
@@ -9,7 +9,7 @@ server.use(express.json());
 server.use(cors());
 
 server.get('/', (req, res) => {
-  res.status(200).json({message: "It's alive!"});
+  res.status(200).json({ message: "It's alive!" });
 });
 
 module.exports = server;
